c715177a56022b9bff21ee862dae5a71a2c7fb84
espindolas95 <espindolasol.ok@hotmail.com>
2025-07-24 16:35:26 +0200

    tests(ho tolto l'endpoint incolatro per errore nel js, non era necesario averlo due volte visto que avevo fatto la chiamata axios)

--- js/script.js
+++ js/script.js
@@ -1,42 +1,3 @@
-[
-    {
-        "id": 1,
-        "title": "Skate Park",
-        "date": "01-07-2024",
-        "url": "https://marcolanci.it/boolean/assets/pictures/1.png"
-    },
-    {
-        "id": 2,
-        "title": "Passeggiata",
-        "date": "16-07-2024",
-        "url": "https://marcolanci.it/boolean/assets/pictures/2.png"
-    },
-    {
-        "id": 3,
-        "title": "Alpi",
-        "date": "01-07-2024",
-        "url": "https://marcolanci.it/boolean/assets/pictures/3.png"
-    },
-    {
-        "id": 4,
-        "title": "Sagra",
-        "date": "21-08-2024",
-        "url": "https://marcolanci.it/boolean/assets/pictures/4.png"
-    },
-    {
-        "id": 5,
-        "title": "Watergun",
-        "date": "23-08-2024",
-        "url": "https://marcolanci.it/boolean/assets/pictures/5.png"
-    },
-    {
-        "id": 6,
-        "title": "Riviera",
-        "date": "30-08-2024",
-        "url": "https://marcolanci.it/boolean/assets/pictures/6.png"
-    }
-]
-
 
 document.addEventListener('DOMContentLoaded', () =>{
 
@@ -46,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     //faccio la richiesta http all' api usando axios
     axios.get('https://lanciweb.github.io/demo/api/pictures/').then(response=>{
 
-        //controlando, response.data contiene un array 
+        //controlando, response.data, contiene un array 
         const photos = response.data;
         //per ogni oggetto
         photos.forEach(photo => {
